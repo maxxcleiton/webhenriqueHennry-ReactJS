660ca07cb4dceb2c9ee34d6bbf55352eefb854da
maxxcleiton <maxcleiton@live.com>
2022-09-15 10:30:52 -0300

    update - adicao de links

--- src/App.js
+++ src/App.js
@@ -16,55 +16,66 @@ function App() {
       <header>
         <div className="logo">
           <img className="logo-img" alt="Logomarca do site" src={logo} />
-          <ul className="">
+          <ul className="ul-menu">
             <a href="/">
               <li>PÁGINA INICIAL</li>
             </a>
             <li>CURSOS DISPONÍVEIS</li>
+            <a href="#quemSomosNos">
             <li>QUEM SOMOS NÓS</li>
+            </a>
+            <a href="#contato">
             <li>CONTATO</li>
+            </a>
           </ul>
         </div>
       </header>
 
       <main>
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner1} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner2} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner3} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner4} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner5} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner6} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank"></a>
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
           <img className="banners-img" src={banner7} alt="Garanta sua vaga!" />
+          </a>
         </div>
 
         <div className="banners">
-          <a href="https://t.ly/bQBm" target="_blank">
+          <a href="https://t.ly/bQBm" target="_blank" rel="noreferrer">
             <img className="banners-img" src={banner8} alt="Garanta sua vaga!" />
           </a>
         </div>
@@ -73,21 +84,26 @@ function App() {
       <footer>
         <div className="footer-both">
           <div>
-            <text className="footer-title">QUEM SOMOS NÓS</text>
+            <text className="footer-title" id="quemSomosNos">QUEM SOMOS NÓS</text>
             <span>
               Somos um Colégio de ensino Técnico focado em preparar
               profissionais para as exigências
-              <br /> do mercado de trabalho. Modelo de ensino: presencial.
-              <br></br>
-              Endereço:<br></br>
-              <a href="https://t.ly/UZPn">
+              <br/> do mercado de trabalho. Modelo de ensino: presencial.
+              <br/>
+              <br/>
+              Endereço:<br/>
+              <a 
+              className="find-us-link"
+              href="https://t.ly/UZPn" target="_blank" rel="noreferrer">
                 R. Lagarto, 1021 - Centro, Aracaju - SE, 49010-390
               </a>
             </span>
           </div>
 
           <div>
-            <text className="footer-title">FALE CONOSCO</text>
+            <text 
+            className="footer-title"
+            id="contato">FALE CONOSCO</text>
             <span>
               📞 (79) 3222-9567
               <br />
